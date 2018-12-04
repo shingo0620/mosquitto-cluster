08300494d1559f99b02d99a245021fe8f8a568ee
Shingo <shingo0620@gmail.com>
2018-12-04 23:05:03 +0800

    Update for windows

--- pub_gitbash.sh
+++ pub_gitbash.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker run -it --rm \
+winpty docker run -it --rm \
 --network mosquitto-cluster_mqtt \
 efrecon/mqtt-client pub -h master -t hello -m "HELLO Master"
 
-docker run -it --rm \
+winpty docker run -it --rm \
 --network mosquitto-cluster_mqtt \
 efrecon/mqtt-client pub -h slave -t hello -m "HELLO Slave"
