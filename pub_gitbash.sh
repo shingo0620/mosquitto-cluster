babc553d04c3bb947f7496c4ea7758d1859df32e
Shingo <shingo0620@gmail.com>
2018-12-07 17:25:08 +0800

    Add slave2

--- pub_gitbash.sh
+++ pub_gitbash.sh
@@ -2,9 +2,12 @@
 
 winpty docker run -it --rm \
 --network mosquitto-cluster_mqtt \
-efrecon/mqtt-client pub -h slave -t message -m "HELLO Slave"
+efrecon/mqtt-client pub -h master -t master/message -m "HELLO Master"
 
 winpty docker run -it --rm \
 --network mosquitto-cluster_mqtt \
-efrecon/mqtt-client pub -h master -t message -m "HELLO Master"
+efrecon/mqtt-client pub -h slave1 -t slave1/message -m "HELLO Slave1"
 
+winpty docker run -it --rm \
+--network mosquitto-cluster_mqtt \
+efrecon/mqtt-client pub -h slave2 -t slave2/message -m "HELLO Slave2"
--- sub.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker run -it --link mqtt-master:mqtt-master --rm --name=mqtt-sub efrecon/mqtt-client sub -h mqtt-master -t "#" -v
-exit 0
